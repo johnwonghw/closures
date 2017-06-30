60139460aeb3fcc90a34482275153fc75900d550
John <john.wong.h@gmail.com>
2017-06-30 02:20:21 +0000

    created working makeloadeddie.js, countdowngen.js, logging.js.

---
+++ makeloadeddie.js
@@ -0,0 +1,19 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var i = -1
+
+  /* your code here */
+  return function() {
+    // list.forEach(function(item, i) {
+    // })
+    i ++;
+    return list[i];
+    /* your code here */
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
